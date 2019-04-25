41ec27c43bfb560016c6dfce97eed97a264c235a
deepm89 <deepikamohan89@gmail.com>
2019-04-25 12:27:51 +0000

    maven project

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+import java.time.LocalDate;  
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+        System.out.println(java.time.LocalDate.now()); 
+     
+    }
+}
+
